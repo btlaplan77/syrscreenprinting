f856ab191bc1841f59fe9212041668bb861035c0
tyler rand <tprand@syr.edu>
2013-07-09 19:20:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syrscreenprinting::Application.config.secret_token = '49f580abdff89ac10a4a0b944a0c0b1fe1cf783f7923733723e270715bd39c1fb6aa5717f1ef76a56a3778355ab2cc887066c08a8f13f9ba75c67c6599926e4d'
